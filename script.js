1222ac6115cd11706ca8d5ce0786375b9abb42a6
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-01-27 15:51:07 +0100

    oops

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
     document.getElementById("run").addEventListener("click", function() {
         let ingredientsInput = document.getElementById("ingredientsInput").value;
         async function getRecipes() {
-            let path = `http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3`;
+            let path = "http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3";
             const recipes = await fetch(path, { mode: 'no-cors', headers: {'accept': '*/*'}});
             const data = await recipes.json();
-            console.log(data);
+            await console.log(recipes);
         }
         getRecipes();
     })
+
 })();
