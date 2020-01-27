7616339505fb5fce00f9c4fe3da901901ade8f6d
Neil <neil.mf.benin@gmail.com>
2020-01-27 15:53:46 +0100

    basic search

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const APPID = "app_id=dead107b&app_key=f41a8806635125b308ec8fb021456e20";
 (function () {
     document.getElementById("run").addEventListener("click", function() {
         let ingredientsInput = document.getElementById("ingredientsInput").value;
-        getRecipes();
+        getRecipes(ingredientsInput);
     });
 })();
 
-async function getRecipes() {
-    let path = "https://api.edamam.com/search?q=chicken,carrot,potato&" + APPID + "&from=0&to=3&calories=591-722&health=alcohol-free";
+async function getRecipes(ingredient) {
+    let path = "https://api.edamam.com/search?q=" + ingredient + "&" + APPID + "&from=0&to=3&calories=591-722&health=alcohol-free";
     const recipes = await fetch(path);
     const data = await recipes.json();
     console.log(data);
