97154f50e89e80927e02e582d482eacab0b7ab08
Neil <neil.mf.benin@gmail.com>
2020-01-28 12:07:03 +0100

    getting close to spotify

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const APPID = "app_id=dead107b&app_key=f41a8806635125b308ec8fb021456e20";
-const SPOTIFYCLIENTID = "8f700bce8751463db952c79260589c04";
 const SPOTIFYSECRETID = "bbda1903d8584c76bcb59a98ba731031";
+const SPOTIFYCLIENTID = "client_id=8f700bce8751463db952c79260589c04";
+const AUTH_BASE_URL = 'https://accounts.spotify.com/authorize';
+const REDIRECT_URI = 'redirect_uri=http://localhost:12345/';
+const TOKEN_TYPE = 'response_type=token';
 
 (function () {
     document.getElementById("run").addEventListener("click", function() {
         let ingredientsInput = document.getElementById("ingredientsInput").value;
-        getRecipes(ingredientsInput);
+        //getRecipes(ingredientsInput);
+        getSpotify(ingredientsInput).catch(error => {
+            console.log(error);
+        });
     });
 })();
 
@@ -14,4 +20,22 @@ async function getRecipes(ingredient) {
     const recipes = await fetch(path);
     const data = await recipes.json();
     console.log(data);
+}
+
+async function getSpotify(ingredient) {
+    let path = buildLink();
+    console.log(path);
+    loginSpotify(path);
+    console.log(playlists);
+}
+
+function buildLink() {
+    let link = AUTH_BASE_URL + '?' + SPOTIFYCLIENTID + '&' + REDIRECT_URI + '&' + TOKEN_TYPE;
+    return link;
+}
+
+function loginSpotify(path) {
+    let popup = window.open(path, 'Login in with Spotify', 'width=600, height=400');
+    
+
 }
